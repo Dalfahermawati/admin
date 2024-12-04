7162d6be5b4a0408b54b944d088aea836d2f0ea7
Dalfahermawati <dalfahermawati770@gmail.com>
2024-12-04 09:39:24 +0700

    menambah data form stok

--- main.js
+++ main.js
@@ -19,3 +19,38 @@ const firebaseConfig = {
   messagingSenderId: "390685080124",
   appId: "1:390685080124:web:6a69ed5fd39c3fc21da139",
 };
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export async function ambilDataStok() {
+  const refDokumen = collection(db, "data-stok");
+  const kueri = query(refDokumen, orderBy("namaBarang"));
+  const cuplikanKueri = await getDocs(kueri);
+  let hasil = [];
+  cuplikanKueri.forEach((dok) => {
+    hasil.push({
+      id: dok.id,
+      namaBarang: dok.data().namaBarang,
+      keluar: dok.data().keluar,
+      masuk: dok.data().masuk,
+      jumlahStok: dok.data().jumlahStok,
+    });
+  });
+  return hasil;
+}
+export function formatAngka(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+export async function tambahDataStok(namaBarang, keluar, masuk, jumlahStok) {
+  try {
+    const dokRef = await addDoc(collection(db, 'data-stok'), {
+      namaBarang: namaBarang,
+      keluar: keluar,
+      masuk: masuk,
+      jumlahStok: jumlahStok
+    });
+    console.log('berhasil menambah data-stok ' + dokRef.id);
+  }
+  catch (e) {
+    console.log('gagal menambah data-stok ' + e);
+  }
+}
